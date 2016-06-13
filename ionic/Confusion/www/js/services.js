a854575f2a2c4bc21fda19dc845b189d480e0d65
amrme <amr.e.dev@gmail.com>
2016-06-13 18:52:09 +0200

    implemented the functions in the favorite factory needed for deletion and listing favs

--- ionic/Confusion/www/js/services.js
+++ ionic/Confusion/www/js/services.js
@@ -48,7 +48,7 @@ angular.module('conFusion.services', ['ngResource'])
         .factory('favoriteFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
           var favFac = {};
-          
+
           // to store all the fav dishes
           var favorites = [];
 
@@ -60,6 +60,20 @@ angular.module('conFusion.services', ['ngResource'])
             // to add it if it's not there
             favorites.push({id: index});
           };
+
+          // to delete a dish from the favorites
+          favFac.deleteFromFavorites = function (index) {
+              for (var i = 0; i < favorites.length; i++) {
+                  if (favorites[i].id == index) {
+                      favorites.splice(i, 1);
+                  }
+              }
+          };
+
+        // to get the list of all the favorites
+        favFac.getFavorites = function () {
+            return favorites;
+        };
 
           return favFac;
         }])
